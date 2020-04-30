773fbf3af1c88387168d90e68462cb055e6d033a
MaikPython <soeoskarpaul@gmail.com>
2020-04-30 13:44:59 +0300

    remove marker

--- trackingapp/src/components/Map.jsx
+++ trackingapp/src/components/Map.jsx
@@ -11,11 +11,6 @@ const App = () => {
         attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={position}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
     </Map>
   )
 }
